2d81046777fa2d405f5be4ea400a734f6d8e8463
jgcruz81 <juangcruz81@gmail.com>
2025-03-21 11:11:08 -0400

    Initial Commit

---
+++ Bills/BillsApp.swift
@@ -0,0 +1,17 @@
+//
+//  BillsApp.swift
+//  Bills
+//
+//  Created by Juan Cruz on 3/21/25.
+//
+
+import SwiftUI
+
+@main
+struct BillsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BillsTests/BillsTests.swift
@@ -0,0 +1,17 @@
+//
+//  BillsTests.swift
+//  BillsTests
+//
+//  Created by Juan Cruz on 3/21/25.
+//
+
+import Testing
+@testable import Bills
+
+struct BillsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
